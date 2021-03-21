965739bdc47c51b6bf4e019714d1aa7bc5b48082
Steventime <32660539+Steventime0430@users.noreply.github.com>
2021-03-21 22:04:14 +0800

    Revised Substitution_Cipher and Permutation Cipher

--- substitution_cipher.rb
+++ substitution_cipher.rb
@@ -7,12 +7,10 @@ module SubstitutionCipher
     # Returns: String
     def self.encrypt(document, key)
       # TODO: encrypt string using caesar cipher
-      encrypted_SubstitutionCipher_document = document
-                            .chars
-                            .map { |i| (i.ord + key).chr }
-                            .join
+      document.to_s.chars
+              .map { |i| (i.ord + key).chr }
+              .join
     end
-
     # Decrypts String document using integer key
     # Arguments:
     #   document: String
@@ -20,10 +18,9 @@ module SubstitutionCipher
     # Returns: String
     def self.decrypt(document, key)
       # TODO: decrypt string using caesar cipher
-      decrpted_SubstitutionCipher_document = encrypted_SubstitutionCipher_document
-                            .chars
-                            .map { |i| (i.ord - key).chr }
-                            .join
+      document.to_s.chars
+              .map { |i| (i.ord - key).chr }
+              .join
 
     end
   end
@@ -37,10 +34,9 @@ module SubstitutionCipher
     def self.encrypt(document, key)
       # TODO: encrypt string using a permutation cipher
       look_up_table = (0..127).to_a.shuffle(random: Random.new(key))
-      encrypted_PermutationCipher_document = document
-                            .chars
-                            .map { |i| i = look_up_table[i.ord].chr}
-                            .join
+      document.to_s.chars
+              .map { |i| i = look_up_table[i.ord].chr}
+              .join
     end
 
     # Decrypts String document using integer key
@@ -51,10 +47,9 @@ module SubstitutionCipher
     def self.decrypt(document, key)
       # TODO: decrypt string using a permutation cipher
       look_up_table = (0..127).to_a.shuffle(random: Random.new(key))
-      decrpted_PermutationCipher_document = encrypted_PermutationCipher_document
-                            .chars
-                            .map { |i| i = look_up_table.index(i.ord).chr}
-                            .join
+      document.to_s.chars
+              .map { |i| i = look_up_table.index(i.ord).chr}
+              .join
     end
   end
 end
