8c4cc5301a773d08868a2f244b80b27ffc404ea8
Steventime <32660539+Steventime0430@users.noreply.github.com>
2021-03-22 16:33:15 +0800

    Double_trans Encrpt

--- double_trans_cipher.rb
+++ double_trans_cipher.rb
@@ -8,7 +8,29 @@ module DoubleTranspositionCipher
     # 4. sort columns of each row in predictibly random way
     # 5. return joined cyphertext
 
+    col_num_matrix = Math.sqrt(document.length).ceil
+    matrix = document.chars
+                .each_slice(col_num_matrix)
+                .to_a
+                .tap{ |i| i.last.fill(" ", i.last.length, number_of_matrix - i.last.length) }
 
+    def row_swap(matrix,i,j)
+      matrix[i], matrix[j] = matrix[j], matrix[i]
+      matrix
+    end
+
+    def col_swap(matrix,i,j)
+      matrix.each { |row| row[i], row[j] = row[j], row[i]}
+    end
+
+    Kernel.srand(key)
+    row_swap_key = Array.new(2){ rand(0..matrix.length-1)}
+    Kernel.srand(key)
+    col_swap_key = Array.new(2){ rand(0..matrix[0].length-1)}
+    matrix = row_swap(matrix,row_swap_key[0],row_swap_key[1])
+    matrix = col_swap(matrix,col_swap_key[0],col_swap_key[1])
+    matrix.join
+    
   end
 
   def self.decrypt(ciphertext, key)
--- temp.rb
+++ temp.rb
@@ -11,7 +11,7 @@ number_of_matrix = Math.sqrt(document.length).ceil
 matrix = document.chars
             .each_slice(number_of_matrix)
             .to_a
-            .tap{ |i| i.last.fill(nil, i.last.length, number_of_matrix - i.last.length) }
+            .tap{ |i| i.last.fill(" ", i.last.length, number_of_matrix - i.last.length) }
 
 # Print the original matrix
 print matrix
@@ -27,10 +27,11 @@ def col_swap(matrix,i,j)
 end
 
 Kernel.srand(key)
-a = Array.new(4){ rand(0..number_of_matrix-1)}
-puts a
-matrix = row_swap(matrix,2,1)
-matrix = col_swap(matrix,0,1)
+row_swap_key = Array.new(2){ rand(0..matrix.length-1)}
+Kernel.srand(key)
+col_swap_key = Array.new(2){ rand(0..matrix[0].length-1)}
+matrix = row_swap(matrix,row_swap_key[0],row_swap_key[1])
+matrix = col_swap(matrix,col_swap_key[0],col_swap_key[1])
 
 print matrix
 puts "",matrix.join
